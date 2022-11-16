f5cacd44195b37bdb62a2f3fbf9c137bc7bcd084
Simeon <simeonqs@hotmail.com>
2022-11-16 16:09:23 +0100

    Adding first paths.

--- ANALYSIS/CODE/cockatiel_dataset/00_paths.R
+++ ANALYSIS/CODE/cockatiel_dataset/00_paths.R
@@ -1,8 +1,16 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 14-11-2022
-# Date last modified: 14-11-2022
+# Date last modified: 16-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script provides paths to be sourced in all later scripts 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+path_functions = 'ANALYSIS/CODE/functions'
+path_folders = 'ANALYSIS/DATA/cockatiel_dataset'
+path_chunks = 'ANALYSIS/RESULTS/cockatiel_dataset/chunks'
+path_calls = 'ANALYSIS/RESULTS/cockatiel_dataset/calls'
+path_traces = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/traces.RData'
+path_measurements = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/measurements.RData'
+path_pdf_traces = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/traces.pdf'
+path_pdf_samples_filtering = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/samples filtering.pdf'
