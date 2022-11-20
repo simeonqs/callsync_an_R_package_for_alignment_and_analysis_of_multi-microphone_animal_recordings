9216004b84331d03f21b281077ff241825302aaa
Simeon <simeonqs@hotmail.com>
2022-11-20 11:46:46 +0100

    Adding and updating paths.

--- ANALYSIS/CODE/cockatiel_dataset/00_paths.R
+++ ANALYSIS/CODE/cockatiel_dataset/00_paths.R
@@ -7,10 +7,17 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 path_functions = 'ANALYSIS/CODE/functions'
-path_folders = 'ANALYSIS/DATA/cockatiel_dataset'
+path_recordings = 'ANALYSIS/DATA/cockatiel_dataset'
 path_chunks = 'ANALYSIS/RESULTS/cockatiel_dataset/chunks'
 path_calls = 'ANALYSIS/RESULTS/cockatiel_dataset/calls'
 path_traces = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/traces.RData'
 path_measurements = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/measurements.RData'
 path_pdf_traces = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/traces.pdf'
 path_pdf_samples_filtering = 'ANALYSIS/RESULTS/cockatiel_dataset/traces/samples filtering.pdf'
+path_pdf_samples_spec_objects = 'ANALYSIS/RESULTS/cockatiel_dataset/SPCC/samples spec_objects.pdf'
+path_spcc_results = 'ANALYSIS/RESULTS/cockatiel_dataset/SPCC/spcc_results.RData'
+path_pdf_pco = 'ANALYSIS/RESULTS/cockatiel_dataset/figures/pco.pdf'
+path_pdf_alignment_example = 'ANALYSIS/RESULTS/cockatiel_dataset/figures/alignment example.pdf'
+path_pdf_example_detections = 'ANALYSIS/RESULTS/cockatiel_dataset/figures/detections example.pdf'
+path_pdf_spec_object_example = 'ANALYSIS/RESULTS/cockatiel_dataset/figures/spec_object example.pdf'
+path_pdf_trace_example = 'ANALYSIS/RESULTS/cockatiel_dataset/figures/trace example.pdf'
