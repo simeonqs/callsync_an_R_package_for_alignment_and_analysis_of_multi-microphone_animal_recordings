7da29da6a72fe71c5fd76fbf60948707ecf40fb0
Simeon <simeonqs@hotmail.com>
2022-11-20 11:48:28 +0100

    Moving code to function.

--- ANALYSIS/CODE/cockatiel_dataset/04_SPCC.R
+++ ANALYSIS/CODE/cockatiel_dataset/04_SPCC.R
@@ -1,14 +1,15 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 16-11-2022
-# Date last modified: 16-11-2022
+# Date last modified: 20-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script runs spectrographic cross correlation on single wav files and constructs a 
 # distance matrix based on the output. 
+# source('ANALYSIS/CODE/cockatiel_dataset/04_SPCC.R')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
-libraries = c('tidyverse', 'warbleR', 'oce', 'signal', 'parallel', 'umap')
+libraries = c('tidyverse', 'warbleR', 'oce', 'signal', 'parallel', 'callsync')
 for(i in libraries){
   if(! i %in% installed.packages()) lapply(i, install.packages)
   lapply(libraries, require, character.only = TRUE)
@@ -23,6 +24,18 @@ source('ANALYSIS/CODE/cockatiel_dataset/00_paths.R')
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
+# Settings
+ffilter_from = 500
+freq_range = c(700, 3500)
+plot_it = FALSE
+thr_low = 0.45
+thr_high = 0.6
+wl = 256
+ovl = 250
+method = 'max'
+sum_one = TRUE
+cols = c('#d11141', '#00b159', '#00aedb', '#f37735', '#ffc425')
+
 # Audio files 
 audio_files = list.files(path_calls,  '*wav', full.names = T)
 
@@ -30,38 +43,24 @@ audio_files = list.files(path_calls,  '*wav', full.names = T)
 load(path_measurements)
 audio_files = audio_files[basename(audio_files) %in% measurements$file]
 
-# Generate spec_ojects
-spec_objects = sapply(1:length(audio_files), function(i){
-  file = audio_files[i]
-  wave = readWave(file)
-  wave = ffilter(wave, from = 700, output = 'Wave')
-  spec_object = cutted.spectro(wave, freq_range = c(700, 3500), plot_it = F, 
-                               thr_low = 0.45, thr_high = 0.6, wl = 256, ovl = 250,
-                               method = 'max',
-                               sum_one = T)
-})
+# Load waves
+waves = lapply(audio_files, load.wave, ffilter_from = ffilter_from)
+names(waves) = basename(audio_files)
 
-# Test examples
-#image(t(spec_objects[[1]]), col = hcl.colors(12, 'Blue-Yellow', rev = T)) 
+# Run function
+message('Starting spcc...')
+m = run.spcc(waves,
+             freq_range = freq_range,
+             thr_low = thr_low,
+             thr_high = thr_high,
+             wl = wl,
+             ovl = ovl,
+             method = method,
+             sum_one = sum_one,
+             mc.cores = 4)
 
-# Plot all
-pdf(path_pdf_spec_objects, 6, 3)
-par(mfrow = c(1, 2))
-for(i in 1:length(audio_files)){
-  better.spectro(readWave(audio_files[i]) %>% ffilter(from = 700, output = 'Wave'), 
-                 main = audio_files[i], wl = 256, ovl = 250)
-  image(t(spec_objects[[i]]), col = hcl.colors(12, 'Blue-Yellow', rev = T)) 
-} 
-dev.off()
-
-# Get combinations and run function
-c = combn(1:length(spec_objects), 2)
-o = mclapply(1:ncol(c), function(i)
-  sliding.pixel.comparison(spec_objects[[c[1,i]]], spec_objects[[c[2,i]]], step_size = 10),
-             mc.cores = 4) %>% unlist
-o = o/max(o)
-m = o.to.m(o, str_remove(basename(audio_files), '.wav'))
-save(m, file = path_m)
+# Save
+save(m, file = path_spcc_results)
 
 # Message
 message('Done!')
