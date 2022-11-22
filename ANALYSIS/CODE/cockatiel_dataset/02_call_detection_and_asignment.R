4549d9792a51428c6574107ef2e8e70c89e28202
Simeon <simeonqs@hotmail.com>
2022-11-22 13:32:38 +0100

    Cleaning up.

--- ANALYSIS/CODE/cockatiel_dataset/02_call_detection_and_asignment.R
+++ ANALYSIS/CODE/cockatiel_dataset/02_call_detection_and_asignment.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 14-11-2022
-# Date last modified: 20-11-2022
+# Date last modified: 21-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script loads chunks, detects calls and assigns them to individuals.
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -20,15 +20,19 @@ wing = 10 # how many seconds to load before and after detection for alignment
 keys_id = c('bird_', '_tag')
 keys_rec = c('_\\(', '\\)_')
 all_files = NULL
+save_files = TRUE
+quiet = FALSE
 
 # Run function
-.detections = detect.and.assign(all_files = NULL,
-                                path_chunks = path_chunks,
-                                ffilter_from = ffilter_from, # from where to filter (Hz) the wave when loading
-                                threshold = threshold, # threshold for amplitude envelope when detecting call
-                                msmooth = msmooth, # smoothening settings of amplitude envelope
-                                min_dur = min_dur, # minimal duration for a note to be included in seconds
-                                max_dur = max_dur,
-                                step_size = step_size, # step size in seconds for the alignment
-                                wing = wing) # how many seconds to load before and after detection for alignment
+detect.and.assign(all_files = NULL,
+                  path_chunks = path_chunks,
+                  path_calls = path_calls,
+                  ffilter_from = ffilter_from,
+                  threshold = threshold, 
+                  msmooth = msmooth, 
+                  min_dur = min_dur, 
+                  max_dur = max_dur,
+                  step_size = step_size, 
+                  wing = wing, 
+                  save_files = save_files) 
 
