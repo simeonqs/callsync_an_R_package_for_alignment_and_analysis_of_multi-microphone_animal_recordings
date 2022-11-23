53320f8c282a8492ac12fc6557e4fb43e3d563c3
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-11-23 14:03:19 +0100

    Updating keys and some other settings to new dataset for step 01.

--- ANALYSIS/CODE/cockatiel_dataset/01_major_alignment.R
+++ ANALYSIS/CODE/cockatiel_dataset/01_major_alignment.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 14-11-2022
-# Date last modified: 21-11-2022
+# Date last modified: 22-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script runs the partitioning and alignment of raw recordings.
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -13,13 +13,14 @@ source('ANALYSIS/CODE/cockatiel_dataset/00_set_up.R')
 chunk_size = 15 
 step_size = 0.5
 save_pdf = T 
+chunk_seq = NULL
 keys_rec = c('_\\(', '\\)_')
-keys_id = c('bird_', '_tag')
+keys_id = c('ASWMUX', '.wav')
 blank = 15
 wing = 10
-ffilter_from = 1000
+ffilter_from = NULL
 all_files = list.files(path_recordings, full.names = T, recursive = T)
-all_files = all_files[str_detect(all_files, 'file_3')]
+# all_files = all_files[str_detect(all_files, 'file_3')]
   
 # Run main function
 align(chunk_size = chunk_size,
@@ -27,7 +28,7 @@ align(chunk_size = chunk_size,
       step_size = step_size,
       path_recordings = path_recordings,
       path_chunks = path_chunks, 
-      # chunk_seq = c(15, 30, 45),
+      chunk_seq = chunk_seq,
       keys_id = keys_id,
       keys_rec = keys_rec,
       blank = blank, 
