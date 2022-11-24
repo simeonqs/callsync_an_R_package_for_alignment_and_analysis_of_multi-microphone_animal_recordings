21d80e0c8f68c3bcda83bd034b6b7a54d47a78a5
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-11-24 11:31:12 +0100

    Updating final figure code: - new way of plotting major alignment - new way of plotting detection and assignment

--- ANALYSIS/CODE/05_final_figures.R
+++ ANALYSIS/CODE/05_final_figures.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 17-11-2022
-# Date last modified: 20-11-2022
+# Date last modified: 23-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script plot the final figures for the paper. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -18,77 +18,82 @@ source('ANALYSIS/CODE/00_set_up.R')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Subset to one set of files
-all_files = list.files(path_folders, full.names = T, recursive = T)
-all_files = all_files[str_detect(all_files, '2022_11_10-07_29_59')][c(1, 2)]
+all_files = list.files(path_recordings, full.names = T, recursive = T)
+all_files = all_files[str_detect(all_files, '2021_07_16-05_54_59')]
 
 # Run main function
-chunk_list = align(chunk_size = 2,
+chunk_size = 15 
+step_size = 0.5
+save_pdf = T 
+chunk_seq = 60
+keys_rec = c('_\\(', '\\)_')
+keys_id = c('ASWMUX', '.wav')
+blank = 15
+wing = 10
+ffilter_from = NULL
+chunk_list = align(chunk_size = chunk_size,
                    all_files = all_files,
-                   step_size = 0.5,
-                   path_recordings = NULL,
-                   path_chunks = NULL, 
-                   keys_rec = c('_\\(', '\\)_'),
-                   keys_id = c('bird_', '_tag'),
-                   blank = 15,
-                   wing = 10,
-                   save_pdf = FALSE)
-
-# Plot
-pdf(path_pdf_alignment_example, 10, 5)
-par(mfrow = c(2, 1), mar = c(0, 0, 0, 0), oma = c(5, 1, 1, 1))
-plot(chunk_list$`file_4_(2022_11_10-07_29_59)_ASWMUX209133@01@2022_11_10-07_29_59@75`,
-     xaxt = 'n', yaxt = 'n', xlim = c(0, 60))
-plot(readWave(all_files[2], from = 75, to = 76, units = 'minutes'),
-     col = 'grey', yaxt = 'n', cex.axis = 1)
-mtext('time [s]', 1, 3, cex = 1)
-par(new=TRUE)
-plot(chunk_list$`file_4_(2022_11_10-07_29_59)_ASWMUX209139@02@2022_11_10-07_29_59@75`,
-     xaxt = 'n', yaxt = 'n', bty = 'n', xlim = c(0, 60))
-dev.off()
+                   step_size = step_size,
+                   path_recordings = path_recordings,
+                   path_chunks = path_figures, 
+                   chunk_seq = chunk_seq,
+                   keys_id = keys_id,
+                   keys_rec = keys_rec,
+                   blank = blank, 
+                   wing = wing, 
+                   ffilter_from = ffilter_from,
+                   save_pdf = save_pdf)
+file.rename(path_example_chunk_before, path_example_chunk_after)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Detection and assignment ----
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-# List files and detect recording IDs
+# List files for example
 all_files = list.files(path_chunks, pattern = '*wav', full.names = T, recursive = T)
-all_files = all_files[str_detect(all_files, '2022_11_10-07_29_59@75')][1:2]
+all_files = all_files[str_detect(all_files, '2021_07_16-05_54_59@150')]
 
 # Run function
-detections = detect.and.assign(ffilter_from = 1100,
-                               threshold = 0.4, 
-                               msmooth = c(1000, 95) , 
-                               min_dur = 0.1,
-                               max_dur = 0.3,
-                               step_size = 1/50,
-                               wings = 10,
-                               all_files = all_files,
-                               save_files = FALSE)
-detections$start = detections$start - 0.1*22050
-detections$end = detections$end + 0.1*22050
+ffilter_from = 1100
+threshold = 0.4
+msmooth = c(1000, 95)
+min_dur = 0.1 
+max_dur = 0.3
+step_size = 1/50 
+wing = 10
+keys_id = c('bird_', '_tag')
+keys_rec = c('_\\(', '\\)_')
+save_files = FALSE
+d = detect.and.assign(all_files = all_files,
+                      path_chunks = NULL,
+                      path_calls = NULL,
+                      ffilter_from = ffilter_from,
+                      threshold = threshold, 
+                      msmooth = msmooth, 
+                      min_dur = min_dur, 
+                      max_dur = max_dur,
+                      step_size = step_size, 
+                      wing = wing, 
+                      save_files = save_files) 
 
 # Plot
 pdf(path_pdf_example_detections, 10, 5)
-par(mfrow = c(2, 1), mar = c(0, 0, 0, 0), oma = c(5, 5, 1, 1))
-
-plot(readWave(all_files[1], from = 0, to = 2, units = 'minutes'), xaxt = 'n', xlab = '')
-sub = detections[str_detect(detections$file, '@06@'),]
-abline(v = sub$start/22050, lty = 2, col = 'green', lwd = 2)
-abline(v = sub$end/22050, lty = 2, col = 'green', lwd = 2)
-for(i in 1:nrow(sub)) 
-  rect(xleft = sub$start[i]/22050, xright = sub$end[i]/22050, 
-       ybottom = par("usr")[3], ytop = par("usr")[4],
-       border = NA, col = alpha('green', 0.3))
-
-plot(readWave(all_files[2], from = 0, to = 2, units = 'minutes'))
-sub = detections[str_detect(detections$file, '@03@'),]
-abline(v = sub$start/22050, lty = 2, col = 'green', lwd = 2)
-abline(v = sub$end/22050, lty = 2, col = 'green', lwd = 2)
-for(i in 1:nrow(sub)) 
-  rect(xleft = sub$start[i]/22050, xright = sub$end[i]/22050, 
-       ybottom = par("usr")[3], ytop = par("usr")[4],
-       border = NA, col = alpha('green', 0.3))
-
+par(mfrow = c(6, 1), mar = c(0, 0, 0, 0), oma = c(5, 1, 1, 1))
+for(i in 1:length(all_files)){
+  plot(readWave(all_files[i], from = 7*60+30, to = 8*60+30, units = 'seconds'), 
+       yaxt = 'n', xaxt = 'n', xlab = '')
+  bird = all_files[i] |> strsplit('@') |> sapply(`[`, 2)
+  sub = d[str_detect(d$file, bird),]
+  for(j in 1:nrow(sub)){
+    rect(xleft = sub$start[j]/22050-7*60-30-0.1, xright = sub$end[j]/22050-7*60-30+0.1, 
+         ybottom = par("usr")[3], ytop = par("usr")[4],
+         border = NA, col = alpha('cyan3', 0.3))
+    abline(v = sub$start[j]/22050-7*60-30-0.1, lty = 2, col = 'cyan3', lwd = 2)
+    abline(v = sub$end[j]/22050-7*60-30+0.1, lty = 2, col = 'cyan3', lwd = 2)
+  }
+}
+axis(1, cex.axis = 1.5)
+mtext('time [s]', 1, 3)
 dev.off()
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
