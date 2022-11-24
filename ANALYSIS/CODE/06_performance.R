359d0c21199e2f5f99253ee2353cbfa83bf3bbfe
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-11-24 11:31:29 +0100

    Finishing up the performance code.

--- ANALYSIS/CODE/06_performance.R
+++ ANALYSIS/CODE/06_performance.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 22-11-2022
-# Date last modified: 22-11-2022
+# Date last modified: 23-11-2022
 # Author: Simeon Q. Smeele
 # Description: This script compares the ground truth tables to the detections. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -9,8 +9,20 @@
 # Set-up
 source('ANALYSIS/CODE/00_set_up.R')
 
+# Load measurements for filtering
+load(path_measurements)
+
+# Load ground truth
+gt = load.selection.tables.audacity(path_ground_truth_tables)
+
+# Get the associated chunks
+all_files = list.files(path_chunks, pattern = '*wav', full.names = T, recursive = T)
+path_ground_truth_chunks = all_files[(str_detect(all_files, '@120.wav') | str_detect(all_files, '@135.wav') |
+                                        str_detect(all_files, '@150.wav')) & 
+                                       str_detect(all_files, '2021_07_16-05_54_59')]
+
 # Run detect and assign on ground truth chunks
-d = detect.and.assign(path_chunks = path_ground_truth_chunks,
+d = detect.and.assign(all_files = path_ground_truth_chunks,
                       ffilter_from = 1100,
                       threshold = 0.4, 
                       msmooth = c(1000, 95), 
@@ -18,10 +30,17 @@ d = detect.and.assign(path_chunks = path_ground_truth_chunks,
                       max_dur = 0.3,
                       step_size = 1/50, 
                       wing = 10, 
+                      # path_calls = '~/Desktop',
                       save_files = F)
+
+# Filter for calls and fix start/end
+b = d
+d$fs = sprintf('%s@%s-%s.wav', d$file, d$start, d$end)
+d = d[d$fs %in% measurements$file,]
 d[c('start', 'end')] = d[c('start', 'end')]/22050
 
-# Load ground truth
-gt = load.selection.tables.audacity(path_ground_truth_tables)
+# Calculate performance
+perf = calc.perf(d = d, gt = gt)
 
-
+# Save results
+write.table(perf, file = path_perf)
