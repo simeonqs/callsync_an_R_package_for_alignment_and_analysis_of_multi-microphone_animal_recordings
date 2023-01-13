40104e83cea5e303ceb326dc2119f600f3d93f18
Simeon Q. Smeele <simeonqs@hotmail.com>
2023-01-13 17:04:59 +0100

    Updating some settings in step 02 to improve performance.

--- ANALYSIS/CODE/02_call_detection_and_assignment.R
+++ ANALYSIS/CODE/02_call_detection_and_assignment.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: methods paper
 # Date started: 14-11-2022
-# Date last modified: 18-12-2022
+# Date last modified: 11-01-2023
 # Author: Simeon Q. Smeele
 # Description: This script loads chunks, detects calls and assigns them to individuals.
 # source('ANALYSIS/CODE/02_call_detection_and_assignment.R')
@@ -12,7 +12,7 @@ source('ANALYSIS/CODE/00_set_up.R')
 
 # Settings
 ffilter_from = 1100 
-threshold = 0.4 
+threshold = 0.18
 msmooth = c(1000, 95) 
 min_dur = 0.1 
 max_dur = 0.3
@@ -20,6 +20,7 @@ step_size = 1/50
 wing = 10 
 all_files = NULL
 save_files = TRUE
+save_extra = 0.05
 
 # Run function
 detect.and.assign(all_files = all_files,
@@ -32,4 +33,5 @@ detect.and.assign(all_files = all_files,
                   max_dur = max_dur,
                   step_size = step_size, 
                   wing = wing, 
-                  save_files = save_files) 
+                  save_files = save_files,
+                  save_extra = save_extra) 
